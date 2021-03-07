73789b17687be909ba78864b3ac8de89a3d641a9
Sammy Laureys <sammylaureys@hotmail.com>
2021-03-07 14:32:45 +0100

    first commit

---
+++ MovieSearcher/MovieTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearcher
+//
+//  Created by Davy Laureys on 02/03/2021.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var movieYearLabel: UILabel!
+    @IBOutlet var moviePosterImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    static let indentifier = "MovieTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MovieTableViewCell", bundle: nil)
+    }
+    
+    func configure(with model: Movie){
+        self.movieTitleLabel.text = model.Title
+        self.movieYearLabel.text = model.Year
+        let url = model.Poster
+        if let data = try? Data(contentsOf: URL(string: url)!) {
+            self.moviePosterImageView.image = UIImage(data: data)
+        }
+    }
+
+    
+}
